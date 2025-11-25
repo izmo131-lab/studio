6012e4f8cbf55cfbbad2f39af94dcd8f17f090a4
Izan Molina Loza <izmo131@vidalibarraquer.net>
2025-11-25 15:50:31 +0000

    Try fixing this error: `Console Error: In HTML, <a> cannot be a descenda

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -73,7 +73,7 @@ export default function Header() {
           <NavigationMenuList>
              {mainLinks.map((link) => (
                <NavigationMenuItem key={link.href}>
-                <Link href={link.href} passHref>
+                <Link href={link.href} legacyBehavior passHref>
                   <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                     {link.label}
                   </NavigationMenuLink>
@@ -194,7 +194,7 @@ const ListItem = React.forwardRef<
   return (
     <li>
       <NavigationMenuLink asChild>
-        <a
+        <Link
           ref={ref}
           className={cn(
             "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
@@ -206,7 +206,7 @@ const ListItem = React.forwardRef<
           <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
             {children}
           </p>
-        </a>
+        </Link>
       </NavigationMenuLink>
     </li>
   )
