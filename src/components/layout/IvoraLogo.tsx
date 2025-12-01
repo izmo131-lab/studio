b2f43038aba6e84ef9d5606216214e918dfa0091
Izan Molina Loza <izmo131@vidalibarraquer.net>
2025-12-01 16:48:43 +0000

    no vull que generis cap imatge. nomes que utilitzis l'arxiu que tens a l

--- src/components/layout/IvoraLogo.tsx
+++ src/components/layout/IvoraLogo.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 export default function IvoraLogo({ className }: { className?: string }) {
   return (
     <Image
-      src="/Captura de pantalla 2025-11-26 195640.png"
+      src="/imatge-removebg-preview.png"
       alt="Ivore Logistics Logo"
       width={160}
       height={40}
