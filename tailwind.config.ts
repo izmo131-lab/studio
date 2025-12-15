7acd0ace513520efe43f1fb3b5454f93336f77aa
Izan Molina Loza <izmo131@vidalibarraquer.net>
2025-12-15 17:53:25 +0000

    en apartat de seguiment fes Dissenya una Barra de Progrés Visual (Timeli

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,11 @@ export default {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  safelist: [
+    'bg-yellow-500',
+    'bg-blue-500',
+    'bg-green-500',
+  ],
   theme: {
     extend: {
       fontFamily: {
